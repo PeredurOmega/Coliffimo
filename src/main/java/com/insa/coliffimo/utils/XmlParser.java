e9050b278a5450b01afd3cfc6c1a8f25f15a85bd
LouisePietropaoli <louise.pietropaoli@gmail.com>
2022-03-10 11:36:41 +0100

    add resource files + update parsing

--- src/main/java/com/insa/coliffimo/utils/XmlParser.java
+++ src/main/java/com/insa/coliffimo/utils/XmlParser.java
@@ -17,6 +17,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class XmlParser {
     private static final String INTERSECTION_TAG_NAME = "intersection";
@@ -29,14 +32,15 @@ public class XmlParser {
     private static final String SEGMENT_ORIGIN_ATTRIBUTE_NAME = "origin";
     private static final String SEGMENT_DESTINATION_ATTRIBUTE_NAME = "destination";
 
-    private static final String FILENAME = "/Users/louisepietropaoli/git/pld-agile/fichiersXML/smallMap.xml";
+
+    private static final String XML_MAPS_RESOURCE_FILE_NAME = "smallMap.xml";
 
     /**
-     * @param filePath todo : mentionner source
+     * @brief parse xml file into a map object
+     * @param filePath
      * @source https://mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/
      */
     public Map parse(String filePath) {
-        filePath = FILENAME;
         Map map = new Map();
 
         // Instantiate the Factory
@@ -50,43 +54,27 @@ public class XmlParser {
             // parse XML file
             DocumentBuilder db = dbf.newDocumentBuilder();
 
-            Document doc = db.parse(new File(FILENAME));
+            Document doc = db.parse(getFileFromResource(XML_MAPS_RESOURCE_FILE_NAME));
 
             // optional, but recommended
             // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
             doc.getDocumentElement().normalize();
 
-            addSegmentsToMap(map, doc);
             addIntersectionsToMap(map, doc);
 
-        } catch (ParserConfigurationException | SAXException | IOException e) {
+        } catch (ParserConfigurationException | SAXException | IOException | URISyntaxException e) {
             e.printStackTrace();
         }
 
         return map;
     }
 
-    private void addSegmentsToMap(Map m, Document d)
-    {
-        NodeList nodesIntersection = d.getElementsByTagName(INTERSECTION_TAG_NAME);
-
-        for (int temp = 0; temp < nodesIntersection.getLength(); temp++) {
-            Node node = nodesIntersection.item(temp);
-
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) node;
-
-                Long id = Long.parseLong(element.getAttribute(INTERSECTION_ID_ATTRIBUTE_NAME));
-                float latitude = Float.parseFloat(element.getAttribute(INTERSECTION_LATITUDE_ATTRIBUTE_NAME));
-                float longitude = Float.parseFloat(element.getAttribute(INTERSECTION_LONGITUDE_ATTRIBUTE_NAME));
-
-                m.addIntersection(new Intersection(id, latitude, longitude));
-            }
-        }
-    }
-
-    private void addIntersectionsToMap(Map m, Document d)
-    {
+    /**
+     * Add an intersection to the map from the document
+     * @param m the map to update
+     * @param d the document from which to get informations
+     */
+    private void addIntersectionsToMap(Map m, Document d) {
         NodeList nodesSegment = d.getElementsByTagName(SEGMENT_TAG_NAME);
 
         for (int temp = 0; temp < nodesSegment.getLength(); temp++) {
@@ -105,4 +93,20 @@ public class XmlParser {
         }
     }
 
+    /**
+     * Create a file with the given resource file name
+     * @param fileName the filename of the resource
+     * @return new File from the resource
+     * @throws URISyntaxException
+     */
+    private File getFileFromResource(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found! " + fileName);
+        } else {
+            System.out.println(resource.getPath());
+            return new File(resource.toURI());
+        }
+    }
 }
