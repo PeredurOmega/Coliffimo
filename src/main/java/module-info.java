a9b85ff7c67d1059bb26b285ccc9585ec5df5c7c
pauls <paul.souteyrat@timeplanner-app.com>
2022-03-11 14:10:19 +0100

    Fixing module info - Solve #11

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,9 @@ module com.insa.coliffimo {
     requires javafx.web;
     requires javafx.swing;
     requires MaterialFX;
-    requires com.dlsc.gmapsfx;
+    requires kotlin.stdlib;
 
     opens com.insa.coliffimo to javafx.fxml;
+    opens com.insa.coliffimo.leaflet to javafx.fxml;
     exports com.insa.coliffimo;
 }
