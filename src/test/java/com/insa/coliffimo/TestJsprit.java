fc4272da32f78b9741a83ba4f1383f92c5b9bee5
Getti Laetitia <laetitia.getti@insa-lyon.fr>
2022-03-15 08:40:34 +0100

    test simple pas fonctionnel

--- src/test/java/com/insa/coliffimo/TestJsprit.java
+++ src/test/java/com/insa/coliffimo/TestJsprit.java
@@ -1,9 +1,14 @@
 package com.insa.coliffimo;
 
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.ResponsePath;
 import com.graphhopper.jsprit.core.algorithm.VehicleRoutingAlgorithm;
 import com.graphhopper.jsprit.core.algorithm.box.Jsprit;
 import com.graphhopper.jsprit.core.problem.Location;
 import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
+import com.graphhopper.jsprit.core.problem.cost.VehicleRoutingTransportCosts;
 import com.graphhopper.jsprit.core.problem.job.Service;
 import com.graphhopper.jsprit.core.problem.job.Shipment;
 import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
@@ -11,10 +16,13 @@ import com.graphhopper.jsprit.core.problem.solution.route.activity.TimeWindow;
 import com.graphhopper.jsprit.core.problem.vehicle.VehicleImpl;
 import com.graphhopper.jsprit.core.util.FastVehicleRoutingTransportCostsMatrix;
 import com.graphhopper.jsprit.core.util.Solutions;
+import com.graphhopper.jsprit.core.util.VehicleRoutingTransportCostsMatrix;
+import com.graphhopper.util.*;
 import org.junit.jupiter.api.Test;
 
 
 import java.util.Collection;
+import java.util.Locale;
 
 public class TestJsprit {
 
@@ -36,11 +44,47 @@ public class TestJsprit {
                 .build();
         System.out.println("véhicule créé");
 
+        // Create matrix
+        GraphHopper hopper = new GraphHopper();
+        // Pour chaque couple de destinations = chaque case de la matrice
+        // simple configuration of the request object
+        GHRequest req = new GHRequest(42.508552, 1.532936, 42.507508, 1.528773).
+                // note that we have to specify which profile we are using even when there is only one like here
+                        setProfile("car").
+                // define the language for the turn instructions
+                        setLocale(Locale.US);
+        GHResponse rsp = hopper.route(req);
+
+        // handle errors
+        if (rsp.hasErrors())
+            throw new RuntimeException(rsp.getErrors().toString());
+
+        // use the best path, see the GHResponse class for more possibilities.
+        ResponsePath path = rsp.getBest();
+
+        // points, distance in meters and time in millis of the full path
+        PointList pointList = path.getPoints();
+        double distance = path.getDistance();
+        long timeInMs = path.getTime();
+
+        Translation tr = hopper.getTranslationMap().getWithFallBack(Locale.UK);
+        InstructionList il = path.getInstructions();
+        // iterate over all turn instructions
+        for (Instruction instruction : il) {
+            // System.out.println("distance " + instruction.getDistance() + " for instruction: " + instruction.getTurnDescription(tr));
+        }
+        assert il.size() == 6;
+        assert Helper.round(path.getDistance(), -2) == 900;
+
+
+
+        VehicleRoutingTransportCostsMatrix.Builder costMatrixBuilder = VehicleRoutingTransportCostsMatrix.Builder.newInstance(true);
 
         VehicleRoutingProblem problem = VehicleRoutingProblem.Builder.newInstance()
                 .addJob(shipment).addVehicle(vehicle1)
-                //.setRoutingCost(new FastVehicleRoutingTransportCostsMatrix(getLocations()))
+                .setRoutingCost(costMatrixBuilder.build())
                 .build();
+        System.out.println(problem);
 
         // define an algorithm out of the box - this creates a large neighborhood search algorithm
         VehicleRoutingAlgorithm algorithm = Jsprit.createAlgorithm(problem);
