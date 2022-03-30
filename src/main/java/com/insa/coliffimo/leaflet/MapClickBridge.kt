ca16c50d84b976f9cbd432849fb48eb0bec0f308
pauls <paul.souteyrat@timeplanner-app.com>
2022-03-30 15:20:02 +0200

    Implementing marker addition by clicking on map

--- src/main/java/com/insa/coliffimo/leaflet/MapClickBridge.kt
+++ src/main/java/com/insa/coliffimo/leaflet/MapClickBridge.kt
@@ -1,12 +1,15 @@
 package com.insa.coliffimo.leaflet
 
-class MapClickBridge {
+import com.insa.coliffimo.MainController
 
-    fun callbackMapClick(lat: Double, lng: Double){
-        println("$lat $lng")
+class MapClickBridge(private val mainController: MainController) {
+
+    fun callbackMapClick(lat: Double, lng: Double) {
+        println("$lat $lng marker CREATION")
+        mainController.addPoint(LatLong(lat, lng))
     }
 
-    fun callbackMapDragged(lat: Double, lng: Double, name: String){
+    fun callbackMapDragged(lat: Double, lng: Double, name: String) {
         println("dragged $lat $lng marker $name")
     }
 }
